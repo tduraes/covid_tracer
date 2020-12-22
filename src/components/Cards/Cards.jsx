e775521636ad17a1d78daee7f91c1a16b6f07a56
TGD9 <duraes.tiago@gmail.com>
2020-12-22 10:44:20 +0000

    first working version

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import styles from './Cards.module.css';
+import CountUp from 'react-countup';
+import cx from 'classnames';
+
+
+const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) => {
+	if(!confirmed) {
+		return 'Loading...';
+	}
+
+
+	return (
+		<div className={styles.container}>
+			<Grid container spacing={3} justify="center">
+				<Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>Infetados</Typography>
+						<Typography variant="h5">
+							<CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
+						</Typography>
+						<Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+						<Typography variant="body2">Numero de casos ativos de COVID-19</Typography>
+					</CardContent>
+				</Grid>
+				<Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>Recuperados</Typography>
+						<Typography variant="h5">
+							<CountUp start={0} end={recovered.value} duration={2.5} separator="," />
+						</Typography>
+						<Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+						<Typography variant="body2">Numero de recuperados de COVID-19</Typography>
+					</CardContent>
+				</Grid>
+				<Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>Mortes</Typography>
+						<Typography variant="h5">
+							<CountUp start={0} end={deaths.value} duration={2.5} separator="," />
+						</Typography>
+						<Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+						<Typography variant="body2">Numero de mortes causadas por COVID-19</Typography>
+					</CardContent>
+				</Grid>
+			</Grid>
+		</div>
+		)
+}
+
+export default Cards;
